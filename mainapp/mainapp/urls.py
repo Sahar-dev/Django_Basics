28c91714a98ef43bba1f63a6853f0643cb797131
Sahari_Marzougui <Saha.sahar223@gmail.com>
2024-11-27 16:54:35 +0100

    changes

--- mainapp/mainapp/urls.py
+++ mainapp/mainapp/urls.py
@@ -18,10 +18,16 @@ from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
+
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('subscriptions/', include('subscriptions.urls')),
     path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name='logout'),
     path('finance/', include('finance.urls')),
+    path('sentry-debug/', trigger_error),
 ]
